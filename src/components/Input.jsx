d0ba7df7c3eb19f4cfd76450360ba449edd64f50
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-12-24 16:35:59 +0200

    Styling Input Label & Border When Validating

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 export function Input({ label, register, placeholder, errors }) {
   return (
-      <div className="input">
+      <div className={`input ${errors[register.name] && 'input--red'}`}>
           <label className="poppins-bold">{label}</label>
           <input type="number" {...register} placeholder={placeholder}/>
           {errors[register.name] && <span className="input__error">{errors[register.name].message}</span>}
