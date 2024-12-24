8b7145534c07b9caa1eb447c894c6f25ac6052d5
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-12-24 17:10:03 +0200

    Added Validation to Make Sure Data Is In The Past & Within Range

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form"
 import { Input } from "./Input"
-import arrowIcon from '../../assets/images/icon-arrow.svg'
+import arrowIcon from "../../assets/images/icon-arrow.svg"
 import { AgeOutput } from "./AgeOutput"
 
 export function Form() {
@@ -17,38 +17,65 @@ export function Form() {
 
   return (
     <>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Input
-        register={register("day", { required: "This field is required" })}
-        label="DAY"
-        placeholder="DD"
-        errors={errors}
-      ></Input>
-      
-      <Input
-        register={register("month", { required: "This field is required" })}
-        label="MONTH"
-        placeholder="MM"
-        errors={errors}
-      ></Input>
-
-      <Input
-        register={register("year", { required: "This field is required" })}
-        label="YEAR"
-        placeholder="YYYY"
-        errors={errors}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          register={register("day", {
+            required: "This field is required",
+            min: { value: 1, message: "Must be a valid day" },
+            max: { value: 31, message: "Must be a valid day" },
+          })}
+          label="DAY"
+          placeholder="DD"
+          errors={errors}
         ></Input>
 
-      <input type="submit" />
+        <Input
+          register={register("month", {
+            required: "This field is required",
+            min: { value: 1, message: "Must be a valid month" },
+            max: { value: 12, message: "Must be a valid month" },
+          })}
+          label="MONTH"
+          placeholder="MM"
+          errors={errors}
+        ></Input>
 
-      <div className="form__img">
-        <img src={arrowIcon} alt="icon-arrow" />
-      </div>
+        <Input
+          register={register("year", {
+            required: "This field is required",
+            validate: (value) => {
+              const currentYear = new Date().getFullYear()
+              if (value > currentYear) {
+                return "Must be in the past"
+              } else if (value == currentYear) {
+                const currentMonth = new Date().getMonth() + 1
+                const currentDay = new Date().getDate()
 
-    </form>
-    <AgeOutput title='years'></AgeOutput>
-    <AgeOutput title='months'></AgeOutput>
-    <AgeOutput title='days'></AgeOutput>
+                if (watch("month") > currentMonth) {
+                  return "Must be in the past"
+                } else if (watch("month") == currentMonth) {
+                  if (watch("day") > currentDay) {
+                    return "Must be in the past"
+                  }
+                }
+              }
+              return true
+            },
+          })}
+          label="YEAR"
+          placeholder="YYYY"
+          errors={errors}
+        ></Input>
+
+        <input type="submit" />
+
+        <div className="form__img">
+          <img src={arrowIcon} alt="icon-arrow" />
+        </div>
+      </form>
+      <AgeOutput title="years"></AgeOutput>
+      <AgeOutput title="months"></AgeOutput>
+      <AgeOutput title="days"></AgeOutput>
     </>
   )
 }
